c7807ca5b7affb35651abd62d952b3d3a182c51d
S6ril <cyril.tessier@gmail.com>
2021-04-20 15:04:04 +0200

    passage en yaml

---
+++ src/yaml_utils.py
@@ -0,0 +1,22 @@
+"""
+Programme pour lire les données du json
+"""
+
+import yaml
+
+
+def yaml_data_import(nom_fichier):
+    with open(nom_fichier) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        return data.items()
+
+
+if __name__ == "__main__":
+    nom_fichier = './src/capteur_config.yaml'
+    liste_capteur = yaml_data_import(nom_fichier)
+    
+    for capteur, data in liste_capteur:
+        print(data['type'], " -- ", data['x'], " -- ", data['y'])
+    
+
+
