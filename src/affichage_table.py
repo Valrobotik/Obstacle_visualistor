611a8fadf1c9f15be7c50a280936ea8fb6ef3d9f
S6ril <cyril.tessier@gmail.com>
2021-04-22 20:24:06 +0200

    redefine struc

--- src/affichage_table.py
+++ src/affichage_table.py
@@ -10,36 +10,40 @@ carte = [[0, 0, 3000, 3000, 0],
 
 
 
-    
+def init_plot(ax):
+    ax.plot(carte[0], carte[1])
 
-def update_data():
-    pass
+    point_robot = robot.get_robot_point()
+    plot_robot, = ax.plot(point_robot[0], point_robot[1])
+
+    ax.grid()
+    ax.axis('equal')
+    return plot_robot
 
 
-def update_plot():
-    pass
+def update_data(robot):
+    global x
+    robot.update_position(1500 + 200*np.sin(x), 1000 +
+                          200*np.cos(x), np.arctan2(np.sin(-x), np.cos(-x)))
+    x +=0.1
+
+
+def update_plot(robot, plot_robot):
+    point_robot = robot.get_robot_point()
+    plot_robot.set_data(point_robot[0], point_robot[1])
 
 
 robot = Robot(1500, 1000, np.pi/3)
 
 fig, ax = plt.subplots(1, 1)
-ax.plot(carte[0], carte[1])
-
-point_robot = robot.get_robot_point()
-plot_robot, = ax.plot(point_robot[0], point_robot[1])
-
-
-ax.grid()
-ax.axis('equal')
+plot_robot = init_plot(ax)
 
 x = 0
 while True:
-    robot.update_position(1500 + 200*np.sin(x), 1000 + 200*np.cos(x), np.arctan2(np.sin(-x), np.cos(-x)))
-    point_robot = robot.get_robot_point()
-    plot_robot.set_data(point_robot[0], point_robot[1])
-    x +=0.1
+    update_data(robot)
+    update_plot(robot, plot_robot)
+    
     plt.pause(0.001)
 
 
-plt.show()
 
