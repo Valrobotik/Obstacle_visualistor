35b2666154e386b64eecd2d581964589d321c6f8
S6ril <cyril.tessier@gmail.com>
2021-04-23 13:37:26 +0200

    comment

--- src/robot_package/capteur_creator.py
+++ src/robot_package/capteur_creator.py
@@ -3,6 +3,14 @@ from robot_package.DistSenor import DistSensor
 import numpy as np
 
 def capteur_creator(nom_fichier):
+    """Fonction qui permet de lire le fichier yaml et de retourner seulement les données pour les capteurs
+
+    Args:
+        nom_fichier (str): emplacement du fichier yaml
+
+    Returns:
+        liste Distsensor: liste avec tous les capteurs créer en classe Distsensor
+    """
 
     # initialisation des capteurs
     dist_sensors = []
