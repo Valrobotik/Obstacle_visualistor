a2a674a6a80e03cd2db3d6bb761cf50afc44db91
S6ril <cyril.tessier@gmail.com>
2021-04-24 14:32:19 +0200

    some comment + ajout des dernier points de la table dans get_table_point

--- src/utils/get_table_point.py
+++ src/utils/get_table_point.py
@@ -8,6 +8,14 @@ from utils.yaml_utils import yaml_data_import
 
 
 def get_table_point(nom_fichier):
+    """Fonction pour obtenir les points de la table d'après le fichier configuration yaml
+
+    Args:
+        nom_fichier (string): nom du fichier de configuation yaml
+
+    Returns:
+        liste float: liste des coordonnées de la table
+    """
 
     point_table = yaml_data_import(nom_fichier)
     points = point_table.get('point_table')
@@ -17,6 +25,10 @@ def get_table_point(nom_fichier):
     for point in points:
             listeX.append(points.get(point).get('x'))
             listeY.append(points.get(point).get('y'))
+    
+    # On ajoute les derniers points de la table pour fermer le rectangle
+    listeX.append(listeX[0])
+    listeY.append(listeY[0])
 
     return [listeX, listeY]
 
