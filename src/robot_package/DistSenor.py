aa50e203a794363ec3f68f4e583d55037ea5117b
S6ril <cyril.tessier@gmail.com>
2021-04-24 14:22:28 +0200

    some comment

--- src/robot_package/DistSenor.py
+++ src/robot_package/DistSenor.py
@@ -2,10 +2,10 @@ import numpy as np
 
 class DistSensor(object):
     """
-    capteur de distance
+    classe des capteurs de distance
     """
     def __init__(self, x=0.0, y=0.0, theta=0.0) -> None:
-        """[summary]
+        """focntion de construction de la classe des capteurs de distance
 
         Args:
             x (float): position en x en mm du capteur
@@ -16,8 +16,7 @@ class DistSensor(object):
         self.obstacle_pose = [None, None] # Position de l'obstacle dans le repère du capteur
 
     def set_sensor_pose(self, x, y, theta):
-        """[summary]
-
+        """fonction pour définir l'emplacement du capteur
         Args:
             x (float): position en x en mm du capteur
             y (float): position en y en mm du capteur
@@ -26,16 +25,14 @@ class DistSensor(object):
         self.position = np.array([x, y, theta])
 
     def get_sensor_pose(self):
-        """[summary]
-
+        """Fonction pour obtenir l'emplacement du capteur
         Returns:
             [tuple]: (x, y, t) pose of the sensor in the robot coordinates
         """        
         return self.position
 
     def set_dist(self, distance_x, distance_y):
-        """[summary]
-
+        """Fonction de démo. Permet de donner une mesure au capteur
         Args:
             dist (float): distance en mm de l'obstacle par rapport au capteur
         """
@@ -43,6 +40,10 @@ class DistSensor(object):
         return self.obstacle_pose
 
     def get_transformation_matrix(self):
+        """Fonction pour créer les matrices de projections du repère du capteur au repère du robot
+        Returns:
+            np.array (3,3): Matrice de projetion du repère du capteur au repère du robot
+        """
         # Projection dans le repère du robot mais positionné sur le capteur
         # x (obstacle->capteur)/robot = - distance * sin(theta)
         # y (obstacle->capteur)/robot = distance * cos(theta)
@@ -66,7 +67,9 @@ class DistSensor(object):
 
 
     def get_obstacle_pose(self):
-        """[summary]
+        """Fonction pour preojeter les coordonnées du repère du capteur dans le repère du robot.
+        On utilise des matrices de taille 3x3 pour porjeter des coordonnées plan afin de faire la tranformation par produit matriciel.
+        Pour plus d'information : https://www2.ift.ulaval.ca/~pgiguere/cours/IntroRobotique2017/notes/03-VisionIII.pdf
         Returns:
             [np.array]: position in mm of the obstacle in the robot space (cartesian coordinate)
         """
@@ -80,5 +83,5 @@ class DistSensor(object):
 
         obstace_pose = np.dot(M_robot_centre, obstace_pose)
         
-        # Projection dans le repère du robot au point du capteur puis translation vers le centre du robot
+        # Projection dans le repère du robot avec translation vers le centre du robot
         return np.array([[obstace_pose[0]], [obstace_pose[1]]])
