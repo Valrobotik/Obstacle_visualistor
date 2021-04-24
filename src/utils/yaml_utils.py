2ac76ab47137dbb5ea45e907718529d17b5d4575
S6ril <cyril.tessier@gmail.com>
2021-04-24 14:33:55 +0200

    some comment + cleanup

--- src/utils/yaml_utils.py
+++ src/utils/yaml_utils.py
@@ -6,25 +6,23 @@ import yaml
 
 
 def yaml_data_import(nom_fichier):
+    """Fonction pour importer et lire les fichier yaml
+
+    Args:
+        nom_fichier (string): nom du fichier à importer
+
+    Returns:
+        dict: dictionnaire des donnée chargés
+    """
     with open(nom_fichier) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
         return data
 
 
 if __name__ == "__main__":
-    # nom_fichier = './src/robot_config.yaml'
-    # liste = yaml_data_import(nom_fichier)
-    # print(liste)
+    nom_fichier = './src/robot_config.yaml'
+    liste = yaml_data_import(nom_fichier)
+    print(liste)
     
-    nom_fichier = './src/table_config.yaml'
-    point_table = yaml_data_import(nom_fichier)
-    points = point_table.get('point_table')
-    
-    listeX = []
-    listeY = []
-    for point in points:
-        listeX.append(points.get(point).get('x'))
-        listeY.append(points.get(point).get('y'))
-    
-    print(listeX)
+
     
