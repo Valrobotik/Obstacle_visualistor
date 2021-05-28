64de8fdedb6d2d30a5f3d61d55afb532c134a84a
S6ril <cyril.tessier@gmail.com>
2021-05-28 12:00:07 +0200

    remove unsed function

--- src/utils/find_serial_port.py
+++ src/utils/find_serial_port.py
@@ -4,34 +4,6 @@ import serial
 import serial.tools.list_ports
 
 
-def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
 def user_select_port_loop():
     """Boucle d'attente de l'utilisateur pour obtenir le port de la carte arduino
 
@@ -72,4 +44,6 @@ def user_select_port_loop():
 
 
 if __name__ == '__main__':
-    user_select_port_loop()
+    port = user_select_port_loop()
+    print(port)
+    print(type(port))
